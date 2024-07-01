7cb26a3da8edef714a272d7fd20cbbf8cb7da27b
Gitea <gitea@fake.local>
2024-06-30 23:10:36 -0700

    with test coverage

--- templated_tests/cpp_cli/test_calculator.cpp
+++ templated_tests/cpp_cli/test_calculator.cpp
@@ -5,10 +5,6 @@ TEST(CalculatorTest, HandlesAddition) {
     EXPECT_DOUBLE_EQ(calculate(1, 1, "--add"), 2);
 }
 
-TEST(CalculatorTest, HandlesDivision) {
-    EXPECT_THROW(calculate(1, 0, "--divide"), std::invalid_argument);
-}
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
